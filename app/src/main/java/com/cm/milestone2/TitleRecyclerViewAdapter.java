c56b5554be3993b5f64fb9777517e945d4a2c5c1
Francisco Jesus <franciscojesus0199@gmail.com>
2021-11-12 10:13:28 +0000

    title list interface added
    colors are free to be changed

---
+++ app/src/main/java/com/cm/milestone2/TitleRecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.cm.milestone2;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cm.milestone2.placeholder.PlaceholderContent.PlaceholderItem;
+import com.cm.milestone2.databinding.FragmentTitleBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class TitleRecyclerViewAdapter extends RecyclerView.Adapter<TitleRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public TitleRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentTitleBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(@NonNull FragmentTitleBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
